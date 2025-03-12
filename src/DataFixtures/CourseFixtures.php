ed6d93efe3bdd24f462531efd1ceecdfca937400
dsanchez <dsanchez@campus-eni.fr>
2025-03-12 14:14:20 +0100

    Module 05 Demo03 - Mise en place des fixtures sur l'objet cours.

---
+++ src/DataFixtures/CourseFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Course;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CourseFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        //Création d'un cours
+        $course = new Course();
+        $course->setName('Symfony');
+        $course->setContent('Le développement web côté serveur (avec Symfony)');
+        $course->setDuration(10);
+        $course->setDateCreated(new \DateTimeImmutable('2025-02-01'));
+        $course->setPublished(true);
+        $manager->persist($course);
+
+        $course = new Course();
+        $course->setName('PHP');
+        $course->setContent('Le développement web côté serveur (avec PHP)');
+        $course->setDuration(5);
+        $course->setDateCreated(new \DateTimeImmutable('2025-01-01'));
+        $course->setPublished(true);
+        $manager->persist($course);
+
+        $course = new Course();
+        $course->setName('Apache');
+        $course->setContent('Administration d\'un serveur Apache sous Linux');
+        $course->setDuration(5);
+        $course->setDateCreated(new \DateTimeImmutable('2024-01-01'));
+        $course->setPublished(true);
+        $manager->persist($course);
+
+        //Création de 30 Cours Supplémentaire
+        for($i=1;$i<=30;$i++){
+            $course = new Course();
+            $course->setName("Cours $i");
+            $course->setContent("Description du cours $i");
+            $course->setDuration(mt_rand(1,10));
+            $course->setDateCreated(new \DateTimeImmutable());
+            $course->setPublished(false);
+            $manager->persist($course);
+        }
+
+        $manager->flush();
+    }
+}
